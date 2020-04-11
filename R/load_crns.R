ca1918c6cd3e547dcb63783fe8fbf5fae6e3eae7
Daniel Hocking <djhocking@gmail.com>
2020-04-11 17:07:27 -0400

    added some error handling for reading in crn files and building dataframe. No specific tests yet but works.

--- R/load_crns.R
+++ R/load_crns.R
@@ -1,20 +1,47 @@
-##### fill table with all chronology and lead 1 chronologies
-
-load_crns <- function(dir, crns, lag, lead, include) {
-  
+#' Title
+#'
+#' @param dir path to directory containing chronologies as character string
+#' @param crns vector of character strings containing the names of the chronologies wanting to read into R
+#' @param include 
+#' @param logfile character string indicating path to the log file for errors associated with reading in chronologies. The path is the same as `dir` if not otherwise specified.
+#'
+#' @return dataframe with a year column and columns for each chronology containing values
+#' @export
+#' @importFrom magrittr %>%
+#' @import dplR
+#' 
+#' @details coming soon
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' }
+load_crns <- function(dir, crns, include = NULL, logfile = "read_crns.log") {
+  log_con <- file.path(dir, logfile)
+  cat(paste0("Log File for Reading Chronologies: ", Sys.time()), file = log_con, sep = "\n", append = FALSE)
+  pb <- txtProgressBar(min = 0, max = length(crns), style = 3)
+  j <- 0
   for (i in 1:length(crns)) {
-    chron <- read.crn(paste0(dir, crns[i]))
+    setTxtProgressBar(pb, i)
+    chron <- tryCatch(withCallingHandlers(suppressMessages(read_crn(file.path(dir, crns[i])))), error = function(e) e)
+    if(inherits(chron, "error")) {
+      # warning(paste0("Not able to read chronology ", crns[i], " - skipping"))
+      cat(paste0(crns[i], ": ", chron[["message"]]), file = log_con, sep = "\n", append = TRUE)
+      next
+    } else {
+      j <- j + 1
     colnames(chron)[1] <- crns[i]
-    lag_name <- paste0(file_names_chron[i], "_lag")
-    chron <- rownames_to_column(chron, "year") %>%
+    chron <- tibble::rownames_to_column(chron, "year") %>%
       dplyr::select(-samp.depth) %>%
-      mutate(year = as.integer(as.numeric(year))) %>%
-      mutate(!!lag_name := lead(chron[ ,1], 1))
-    if(i == 1) {
+      dplyr::mutate(year = as.integer(as.numeric(year)))
+    }
+    if(j == 1) {
       all_chronologies <- chron
     } else {
-      all_chronologies <- full_join(all_chronologies, chron, by = "year")
+      all_chronologies <- dplyr::full_join(all_chronologies, chron, by = "year")
     }
   }
-
+  # close(log_con)
+  close(pb)
+  return(all_chronologies)
 }
