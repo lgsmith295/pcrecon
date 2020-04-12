da1ff9e48c25188105108b176bcae6359f415748
Laura Hocking-Smith <lgsmith295@gmail.com>
2020-04-12 12:59:34 -0400

    fixed params to reflect 1 single dataframe as an argument rather than 3 seperate vectors. Still debating whether this is the best way...

--- R/filter_spat.R
+++ R/filter_spat.R
@@ -1,31 +1,44 @@
 
-#' Spatial Filtering of Tree Ring Data 
+#' Spatial Filtering of Tree Ring Data Based on Radius From a Point
 #'
-#' @param ID vector of character strings containing chronology IDs
-#' @param lat numeric vector of latitudes for each chronology
-#' @param lon numeric vector of longitudes for each chronology
-#' @param center center point for calculating radius (radius type)
+#' @param data  dataframe containing columns labelled ID, lat
+#' @param cent_lat latitude of center point
+#' @param cent_lon longitude of center point
 #' @param radius distance from center in km, default 150 (radius type)
-#' @param climate dataframe with climate variables for spatial correlation (footprint type), defaults to NULL 
-#' @param type "radius" or "footprint", defaults to radius
 #'
 #' @return
 #' @export
 #'
 #' @examples
-#' 
-filter_spat <- function(ID, lat, lon, center, radius, climate = NULL, type = "radius") {
-  stores <- sf::st_sfc(st_multipoint(cbind(ID, lon, lat)), crs = 4326)
-  if (type == "radius") {
-    
-    
-    
-  } else {
-    
-    
-    
-    
-    
-  
-  }
+#'
+filter_rad <- function(data, cent_lat, cent_lon, radius, climate = NULL, type = "radius") {
+  # parse dataframe and store as spatial object
+  lat <- data$lat
+  lon <- data$lon
+  ID <- data$ID
+
+  stores <- sf::st_sfc(st_multipoint(cbind(lon, lat)), crs = 4326)
+  me <- st_sfc(st_point(c(cent_lon, cent_lat)), crs = 4326)
+
+  #convert to albers equal area projection
+  stores_aea <- st_transform(stores, "+proj=aea +lat_1=29.5 +lat_2=42.5")
+  me_aea     <- st_transform(me, "+proj=aea +lat_1=29.5 +lat_2=42.5")
+
+  #store as individual points and attach IDs
+  tr_points <- st_cast(stores_aea, "POINT") %>%
+    st_sf(cbind(ID, stores_aea_column))
+
+  # circle from radius and select points within
+  circle <- st_buffer(me_aea, radius)
+  logical <- st_contains(circle, tr_points, sparse = FALSE)
+  crns <- dplyr::filter(names, logical)
+  return (crns)
 }
+
+
+filter_foot <- function(data, footprint, r, alpha)
+
+
+
+
+
