b896fd73c36e2557484658783e0dc1553154ad42
Laura Hocking-Smith <lgsmith295@gmail.com>
2020-04-12 21:03:57 -0400

    created function to filter within set radius. Laura is awesome.

--- R/filter_spat.R
+++ R/filter_spat.R
@@ -1,42 +1,78 @@
 
-#' Spatial Filtering of Tree Ring Data Based on Radius From a Point
+#' Spatial Filtering of Points Based on Radius From a Point
 #'
-#' @param data  dataframe containing columns labelled ID, lat
+#' @param data  dataframe containing columns labeled ID, lat, and lon
 #' @param cent_lat latitude of center point
 #' @param cent_lon longitude of center point
-#' @param radius distance from center in km, default 150 (radius type)
+#' @param radius distance from center in km, default 150
+#'
+#' @return character vector of chronology names within the selected radius
+#' @export
+#'
+#' @examples
+#'
+filter_rad <- function(data, cent_lat, cent_lon, radius = 150, climate = NULL) {
+
+  radius_m <- radius * 1000
+
+  # parse dataframe and store as spatial object
+  data <- data.frame(data, stringsAsFactors = FALSE)
+  lat <- as.numeric(data$lat)
+  lon <- as.numeric(data$lon)
+  ID <- as.character(data$ID)
+
+  stores <- sf::st_sfc(sf::st_multipoint(cbind(lon, lat)), crs = 4326)
+  me <- sf::st_sfc(sf::st_point(c(cent_lon, cent_lat)), crs = 4326)
+
+  # convert to albers equal area projection
+  stores_aea <- sf::st_transform(stores, "+proj=aea +lat_1=29.5 +lat_2=42.5")
+  me_aea     <- sf::st_transform(me, "+proj=aea +lat_1=29.5 +lat_2=42.5")
+
+  # store as individual points and attach IDs
+  tr_points <- sf::st_cast(stores_aea, "POINT")
+  tr_points <- sf::st_sf(cbind(ID, data.frame(tr_points)))
+
+  # circle from radius and select points within
+  circle <- sf::st_buffer(me_aea, radius_m)
+  logical <- sf::st_contains(circle, tr_points, sparse = FALSE)
+  select_crns <- ID[which(logical == TRUE)]
+
+  if(length(select_crns) == 0) warning("No chronologies found within this radius")
+
+  return (select_crns)
+}
+
+
+#' Spatial Filtering of Points Based on Climate Footprint
+#'
+#' @param data dataframe containing columns labeled ID, lat, and lon
+#' @param footprint Raster stack object
+#' @param r minimum correlation coefficient for spatial selection
+#' @param alpha alpha value , default null because you do this when making the footprint in KNMI
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-filter_rad <- function(data, cent_lat, cent_lon, radius, climate = NULL, type = "radius") {
+filter_foot <- function(data, footprint, r, alpha) {
   # parse dataframe and store as spatial object
   lat <- data$lat
   lon <- data$lon
   ID <- data$ID
 
-  stores <- sf::st_sfc(st_multipoint(cbind(lon, lat)), crs = 4326)
-  me <- st_sfc(st_point(c(cent_lon, cent_lat)), crs = 4326)
+  crns <- sf::st_sfc(sf::st_multipoint(cbind(lon, lat)), crs = 4326)
+  stores_proj <- sf::st_transform(crns, ' +proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0')
 
-  #convert to albers equal area projection
-  stores_aea <- st_transform(stores, "+proj=aea +lat_1=29.5 +lat_2=42.5")
-  me_aea     <- st_transform(me, "+proj=aea +lat_1=29.5 +lat_2=42.5")
+  tr_points <- sf::st_cast(stores_proj, "POINT") %>%
+    sf::st_sf(cbind(ID, stores_proj))
 
-  #store as individual points and attach IDs
-  tr_points <- st_cast(stores_aea, "POINT") %>%
-    st_sf(cbind(ID, stores_aea_column))
 
-  # circle from radius and select points within
-  circle <- st_buffer(me_aea, radius)
-  logical <- st_contains(circle, tr_points, sparse = FALSE)
-  crns <- dplyr::filter(names, logical)
-  return (crns)
+
+
+
+
 }
-
-
-filter_foot <- function(data, footprint, r, alpha)
 
 
 
