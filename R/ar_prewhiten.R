e1ede406864e3e9f1ffbf4c4c101e3c6cc2cb9b3
Laura Hocking-Smith <lgsmith295@gmail.com>
2020-04-15 16:34:48 -0400

    returns ar model info when model = TRUE

--- R/ar_prewhiten.R
+++ R/ar_prewhiten.R
@@ -12,9 +12,12 @@ ar_prewhiten <- function (x, model = FALSE, ...)
   y <- x
   idx.goody <- !is.na(y)
   ar <- ar(y[idx.goody])
-  y[idx.goody] <- ar1$resid
-
-  ret <- list(y, ar)
+  y[idx.goody] <- ar$resid
+  if (model == TRUE){
+    ret <- list(y, ar)
+  } else {
+  ret <- y
+  }
   return (ret)
 }
 
