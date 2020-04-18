50c3883aff729ac455aa7891387516e0e6387bcc
Laura Hocking-Smith <lgsmith295@gmail.com>
2020-04-18 17:50:11 -0400

    added stats for calibration period

--- R/perf_stats.R
+++ R/perf_stats.R
@@ -15,16 +15,28 @@
 #' \dontrun{
 #' "coming soon"
 #' }
-perf_stats <- function(valid_est, observed, valid_yrs = valid, calib_yrs = calib, mod_id = "m") {
+perf_stats <- function(valid_est, calib_est, observed, valid_yrs = valid, calib_yrs = calib, mod_id = "m") {
   if(!all(colnames(observed) == c("year", "values"))) {
     stop("observed must be a dataframe that includes the columns year and values")
   }
   x_valid <- dplyr::filter(observed, observed$year %in% valid_yrs)
   x_calib <- dplyr::filter(observed, observed$year %in% calib_yrs)
+
   R2 <- 1 - ((sum((x_valid$values - valid_est$fit)^2)) / (sum((x_valid$values - mean(x_valid$values))^2)))
   Pearson_R2 <- (sum((x_valid$values - mean(x_valid$values)) * (valid_est$fit - mean(valid_est$fit))))^2 / (sum((x_valid$values - mean(x_valid$values))^2) * sum((valid_est$fit - mean(valid_est$fit))^2))
   RE <- 1 - sum((x_valid$values - valid_est$fit) ^ 2) / sum((x_valid$values - mean(x_calib$values, na.rm = TRUE))^2)
   CE <- 1 - sum((x_valid$values - valid_est$fit) ^ 2) / sum((x_valid$values - mean(x_valid$values)) ^ 2)
-  df <- data.frame(model = mod_id, R2, Pearson_R2, RE, CE, stringsAsFactors = FALSE)
+  df_valid <- data.frame(model = mod_id, R2, Pearson_R2, RE, CE, stringsAsFactors = FALSE)
+
+  R2 <- 1 - ((sum((x_calib$values - calib_est$fit)^2)) / (sum((x_calib$values - mean(x_calib$values))^2)))
+  Pearson_R2 <- (sum((x_calib$values - mean(x_calib$values)) * (calib_est$fit - mean(calib_est$fit))))^2 / (sum((x_calib$values - mean(x_calib$values))^2) * sum((calib_est$fit - mean(calib_est$fit))^2))
+  RE <- 1 - sum((x_calib$values - calib_est$fit) ^ 2) / sum((x_calib$values - mean(x_valid$values, na.rm = TRUE))^2)
+  CE <- 1 - sum((x_calib$values - calib_est$fit) ^ 2) / sum((x_calib$values - mean(x_calib$values)) ^ 2)
+  df_calib <- data.frame(model = mod_id, R2, Pearson_R2, RE, CE, stringsAsFactors = FALSE)
+
+
+  df <- list(validation_stats = df_valid, calibration_stats = df_calib)
   return(df)
 }
+
+
