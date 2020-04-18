6fb7595ea1ffb7fef33ec34da83f54c441f03443
Laura Hocking-Smith <lgsmith295@gmail.com>
2020-04-17 23:25:25 -0400

    option to return just prewhitened values or just ar model or both

--- R/ar_prewhiten.R
+++ R/ar_prewhiten.R
@@ -7,16 +7,20 @@
 #' @return
 #'
 #' @examples
-ar_prewhiten <- function (x, model = FALSE, ...)
+ar_prewhiten <- function (x, return = "both", ...)
 {
   y <- x
   idx.goody <- !is.na(y)
   ar <- ar(y[idx.goody])
-  y[idx.goody] <- ar$resid
-  if (model == TRUE){
+  y[idx.goody] <- ar$resid + ar$x.mean
+  if(return == "resid"){
+    ret <- y
+  }
+  if(return == "model"){
+    ret <- ar
+  }
+  if(return == "both"){
     ret <- list(y, ar)
-  } else {
-  ret <- y
   }
   return (ret)
 }
