80c4ee9dba398fc46885b16ca596775bcf97a8e9
Laura Hocking-Smith <lgsmith295@gmail.com>
2020-04-23 16:18:13 -0400

    fixed lags/leads

--- R/filter_cor.R
+++ R/filter_cor.R
@@ -1,7 +1,7 @@
 #' Filter chronologies based on their correlation with climate data
 #'
 #' @param crns dataframe, such as returned by the load_crns function, where there is a column containing years and one column for each chronology
-#' @param lead single value indicating the maximum number of years to shift chronology data forward. 0 indicates no lead, 2 will evaluate 0, 1, and 2 year leads, etc. Defaults to 1.
+#' @param lag single value indicating the maximum number of years to shift chronology data forward. 0 indicates no lag, 2 will evaluate 0, 1, and 2 year lags, etc. Defaults to 1.
 #' @param clim dataframe, such as returned by the get_clim function, containing columns for year and either: individual months climate data (may be more than one), a sum of >1 months climate data, or the average of >1 months data
 #' @param window years to include in correlation analysis, calibration period in original PCreg software
 #' @param method type of correlation analysis, "pearson" or "spearman". Defaults to "pearson"
@@ -13,7 +13,7 @@
 #' @export
 #'
 #' @examples
-filter_cor <- function(crns, lead = 1, clim, cor.window, type = "pearson", alternative = "two.sided", r = 0.25, alpha = 0.90, prewhiten.crn = TRUE, prewhiten.clim = TRUE, calib = calib, valid = valid, full = full){
+filter_cor <- function(crns, lag = 1, clim, cor.window, type = "pearson", alternative = "two.sided", r = 0.25, alpha = 0.90, prewhiten.crn = TRUE, prewhiten.clim = TRUE, calib = calib, valid = valid, full = full){
 
 if(isTRUE(prewhiten.crn)){
     year <- crns$year
@@ -25,26 +25,30 @@ if(isTRUE(prewhiten.crn)){
 
   df <- window_filter(crns = crns, clim = clim, cor.window = cor.window, calib = calib, valid = valid, full = full)
 
-  leads <- c(0:lead)
 
-  rows <- ncol(df$crn_window) * length(leads)
+  lags <- c(0:lag)
+
+  rows <- ncol(df$crn_window) * length(lags)
 
   cors_table <- as.data.frame(matrix(NA, nrow = rows, ncol = 4))
-  names <-  c("chronology", "leads", "correlation", "p_value")
+  names <-  c("chronology", "lags", "correlation", "p_value")
   colnames(cors_table) <- names
 
   crn_names <- colnames(df$crn_window)
 
  k <- 0
- for (j in 1:length(leads)) {
+ for (j in 1:length(lags)) {
   for (i in 1:length(crn_names)) {
     k <- k + 1
     crn <- as.vector(as.numeric(df$crn_window[ ,i]))
     clim<- as.vector(as.numeric(df$clim_window[ , ]))
-    cor <- cor.test( clim, dplyr::lead(crn, leads[j]), conf.level = alpha, type = type, alternative = alternative)
-    cors_table[k, ] <- cbind(crn_names[i], leads[j], cor$estimate, cor$p.value)
+    cor <- cor.test( clim, dplyr::lag(crn, lags[j]), conf.level = alpha, type = type, alternative = alternative)
+    cors_table[k, ] <- cbind(crn_names[i], lags[j], cor$estimate, cor$p.value)
 }
-}
+ }
+    class(cors_table$p_value) <- "numeric"
+    class(cors_table$correlation) <- "numeric"
+    class(cors_table$lags) <- "integer"
     cors_table_small <- dplyr::filter(cors_table, cors_table$correlation >= r ) %>%
       dplyr::filter(p_value <= 1-alpha)
 
@@ -69,12 +73,12 @@ if(isTRUE(prewhiten.crn)){
 
 
 
-#' Fill table of chronologies and lead chronologies that pass correlation filtering
+#' Fill table of chronologies and lag chronologies that pass correlation filtering
 #'
 #' @param crns
-#' @param cors_table_small dataframe of filtered chronology names, leads, correlation, and p values.
+#' @param cors_table_small dataframe of filtered chronology names, lags, correlation, and p values.
 #'
-#' @return dataframe of chronologies, including lead chronologies, that met threshold correlation values for analysis
+#' @return dataframe of chronologies, including lag chronologies, that met threshold correlation values for analysis
 #'
 #'
 #' @examples
@@ -82,10 +86,10 @@ if(isTRUE(prewhiten.crn)){
 crns_table <- function(crns = crns, cors_table_small = cors_table_small) {
  for (i in 1:nrow(cors_table_small)) {
   chron <- crns[ ,cors_table_small$chronology[i]]
-  if (cors_table_small$leads[i] != 0) {
-    lead_val <- as.integer(cors_table_small$leads[i])
+  if (cors_table_small$lags[i] != 0) {
+    lead_val <- as.integer(cors_table_small$lags[i])
     chronology_name <- paste0(cors_table_small$chronology[i], "_lag", lead_val)
-    chron_lead <- dplyr::lead(chron, lead_val )
+    chron_lead <- dplyr::lag(chron, lead_val )
     chron <- data.frame(chron_lead)
     colnames(chron) <- chronology_name
   } else {
