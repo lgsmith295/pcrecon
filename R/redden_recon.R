fa9b8816ca6400078648086ac67e05a80085d66a
Laura Hocking-Smith <lgsmith295@gmail.com>
2020-04-27 20:36:36 -0400

    calculate residuals when ar 0 order in original climate

--- R/redden_recon.R
+++ R/redden_recon.R
@@ -1,6 +1,10 @@
 redden_recon <- function(recon, ar.model) {
   if(ar.model$order == 0) {
-    warning("the ar order for the original climate data was 0, meaning that no autoregressive structure has been added back in (not reddened)")
+    warning("the ar order for the original climate data was 0, meaning that no autoregressive structure has been added back in to the reconstruction (not reddened). If red noise was found in reconstruction, residual (whitened) reconstruction included in recon dataframe")
+    ar <- ar(recon$fit)
+  if(ar$order != 0) {recon$reds <- ar$resid + ar$x.mean
+  }
+
   } else {
     recon <- recon %>%
       dplyr::arrange(year)
