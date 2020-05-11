de5fa3e84d149ae18def8999379a4975db8f57e3
Daniel Hocking <djhocking@gmail.com>
2020-05-11 10:14:19 -0400

    fixed performance stats even if still messy

--- R/perf_stats.R
+++ R/perf_stats.R
@@ -13,36 +13,45 @@
 #'
 #' @examples
 #' \dontrun{
-#' "coming soon"
+#' set.seed(92831)
+#' df <- data.frame(year = 1:20, values = rnorm(20), fit = rnorm(20))
+#' perf_stats(valid_est = df[1:10, c("year", "fit")], calib_est = df[11:20, c("year", "fit")], observed = df[1:20 , c("year", "values")], valid_yrs = 1:10, calib_yrs = 11:20)
+#'
+#' df[19, "values"] <- NA_real_
+#' perf_stats(valid_est = df[1:10, c("year", "fit")], calib_est = df[11:20, c("year", "fit")], observed = df[1:20 , c("year", "values")], valid_yrs = 1:10, calib_yrs = 11:20)
 #' }
 perf_stats <- function(valid_est, calib_est, observed, valid_yrs = valid, calib_yrs = calib, mod_id = "m") {
   if(!all(colnames(observed) == c("year", "values"))) {
     stop("observed must be a dataframe that includes the columns year and values")
   }
+  # add warning if removing values
+
   x_valid <- dplyr::filter(observed, observed$year %in% valid_yrs)
   x_calib <- dplyr::filter(observed, observed$year %in% calib_yrs)
 
   # Validation
-  R2 <- (sum((x_valid$fit - mean(x_valid$values, na.rm = TRUE))^2, na.rm = TRUE)) / (sum((x_valid$values - mean(x_valid$values, na.rm = TRUE))^2, na.rm = TRUE))
+  R2 <- cor(valid_est$fit, x_valid$values, use = "pairwise.complete.obs")^2
+  # R2 <- (sum((valid_est$fit - mean(x_valid$values, na.rm = TRUE))^2, na.rm = TRUE)) / (sum((x_valid$values - mean(x_valid$values, na.rm = TRUE))^2, na.rm = TRUE))
 
-  Pearson_R2 <- (sum((x_valid$values - mean(x_valid$values)) * (valid_est$fit - mean(valid_est$fit))))^2 / (sum((x_valid$values - mean(x_valid$values))^2) * sum((valid_est$fit - mean(valid_est$fit))^2))
+  # Pearson_R2 <- (sum((x_valid$values - mean(x_valid$values)) * (valid_est$fit - mean(valid_est$fit))))^2 / (sum((x_valid$values - mean(x_valid$values))^2) * sum((valid_est$fit - mean(valid_est$fit))^2))
 
-  RE <- 1 - sum((x_valid$values - valid_est$fit) ^ 2) / sum((x_valid$values - mean(x_calib$values))^2)
+  RE <- 1 - sum((x_valid$values - valid_est$fit) ^ 2, na.rm = TRUE) / sum((x_valid$values - mean(x_calib$values, na.rm = TRUE))^2, na.rm = TRUE)
 
-  CE <- 1 - sum((x_valid$values - valid_est$fit) ^ 2) / sum((x_valid$values - mean(x_valid$values)) ^ 2)
+  CE <- 1 - sum((x_valid$values - valid_est$fit) ^ 2, na.rm = TRUE) / sum((x_valid$values - mean(x_valid$values, na.rm = TRUE)) ^ 2, na.rm = TRUE)
 
-  df_valid <- data.frame(model = mod_id, R2, Pearson_R2, RE, CE, stringsAsFactors = FALSE)
+  df_valid <- data.frame(model = mod_id, R2, RE, CE, stringsAsFactors = FALSE)
 
   # Calib
-  R2 <- (sum((x_calib$fit - mean(x_calib$values))^2)) / (sum((x_calib$values - mean(x_calib$values))^2))
+  R2 <- cor(calib_est$fit, x_calib$values, use = "pairwise.complete.obs")^2
+  # R2 <- (sum((x_calib$fit - mean(x_calib$values))^2)) / (sum((x_calib$values - mean(x_calib$values))^2))
+  #
+  # Pearson_R2 <- (sum((x_calib$values - mean(x_calib$values)) * (calib_est$fit - mean(calib_est$fit))))^2 / (sum((x_calib$values - mean(x_calib$values))^2) * sum((calib_est$fit - mean(calib_est$fit))^2))
 
-  Pearson_R2 <- (sum((x_calib$values - mean(x_calib$values)) * (calib_est$fit - mean(calib_est$fit))))^2 / (sum((x_calib$values - mean(x_calib$values))^2) * sum((calib_est$fit - mean(calib_est$fit))^2))
+  RE <- 1 - sum((x_calib$values - calib_est$fit) ^ 2, na.rm = TRUE) / sum((x_calib$values - mean(x_valid$values, na.rm = TRUE))^2, na.rm = TRUE)
 
-  RE <- 1 - sum((x_calib$values - calib_est$fit) ^ 2) / sum((x_calib$values - mean(x_valid$values))^2)
+  CE <- 1 - sum((x_calib$values - calib_est$fit) ^ 2, na.rm = TRUE) / sum((x_calib$values - mean(x_calib$values, na.rm = TRUE)) ^ 2, na.rm = TRUE)
 
-  CE <- 1 - sum((x_calib$values - calib_est$fit) ^ 2) / sum((x_calib$values - mean(x_calib$values)) ^ 2)
-
-  df_calib <- data.frame(model = mod_id, R2, Pearson_R2, RE, CE, stringsAsFactors = FALSE)
+  df_calib <- data.frame(model = mod_id, R2, RE, CE, stringsAsFactors = FALSE)
 
 
   df <- list(validation_stats = df_valid, calibration_stats = df_calib)
