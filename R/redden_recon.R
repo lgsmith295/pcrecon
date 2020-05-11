dd175c01848b4d0c164c0752f59fc5d0819f8fb1
Laura Hocking-Smith <lgsmith295@gmail.com>
2020-05-11 02:18:39 -0400

    fixed if-else

--- R/redden_recon.R
+++ R/redden_recon.R
@@ -2,9 +2,7 @@ redden_recon <- function(recon, ar.model) {
   if(ar.model$order == 0) {
     warning("the ar order for the original climate data was 0, meaning that no autoregressive structure has been added back in to the reconstruction (not reddened). If red noise was found in reconstruction, residual (whitened) reconstruction included in recon dataframe")
     ar <- ar(recon$fit)
-  if(ar$order != 0) {recon$reds <- ar$resid + ar$x.mean
-  }
-
+    recon$reds <- ar$resid + ar$x.mean
   } else {
     recon <- recon %>%
       dplyr::arrange(year)
@@ -18,10 +16,9 @@ redden_recon <- function(recon, ar.model) {
       }
       recon$reds[i] <- recon$fit[i] + tmp
     }
-    recon <- recon %>%
-      dplyr::arrange(dplyr::desc(year))
   }
+  recon <- recon %>%
+    dplyr::arrange(dplyr::desc(year))
   return(recon)
 }
-
 
