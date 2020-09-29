f77f070858749d3e89b0775301fdca6470a50b93
Laura Hocking-Smith <lgsmith295@gmail.com>
2020-09-29 08:07:58 -0400

    data, vignette, description updates

--- analysis/informal/sandbox2.R
+++ analysis/informal/sandbox2.R
@@ -4,34 +4,21 @@ devtools::load_all()
 
 ## parse json files from metadata folder
 metadata <- parse_json(dir = system.file("extdata/metadata", package = "pcreg", mustWork = TRUE), center = TRUE)
+metadata <- metadata[-58, ]
 
 ## filter chronologies based on radius from a point
-select_crns_rad <- filter_rad(data = metadata, cent_lat = 36.21, cent_lon = -105.9, radius = 75)
-metadata_small <- metadata %>%
-  dplyr::filter(ID %in% select_crns_fp)
-
+select_crns_rad <- filter_rad(data = metadata, cent_lat = 36.21, cent_lon = -105.9, radius = 150)
 
 ## filter based on climate footprint
 ## first upload climate footprint, which is a netCDF file downloaded from KNMI climate explorer
 footprint <- raster::raster("inst/extdata/gallinas_cf.nc")
-raster::plot(footprint, col = hcl.colors(n =6))
 select_crns_fp <- filter_foot(data = metadata, footprint = footprint, r = 0.5)
-
-## Map
-
-#ggplot(metadata2, aes(long, lat)) + geom_raster(fortify(footprint))
 
 ## build large dataframe of chronologies selected from the spatial filter
 crns_df <- load_crns(dir = system.file("extdata/crns", package = "pcreg", mustWork = TRUE), crns = select_crns_fp)
 crns_df <- dplyr::arrange(crns_df, year)
 
-## load in climate and rename columns
-# clim2 <- read.table("inst/extdata/nm_clim.txt")
-
-# clim <- clim2[-1, ]
-
-
-climate <- read.csv(system.file("extdata/gallinas_flow.csv", package = "pcreg"))
+climate <- read.csv("inst/extdata/gallinas_flow.csv")
 names(climate) <- c("year", c(1:12))
 clim_long <- climate %>%
   tidyr::pivot_longer(cols = -year, names_to = "month") %>%
@@ -42,29 +29,7 @@ clim_long$value <- round(clim_long$log_clim, 4)
 
 clim_long <- clim_long[ ,-3]
 
-# gdata::write.fwf(x = data.frame(clim), file = "clm_test.txt", width = c(c(9, rep(7, 12))))
-#
-# table <- read.table("inst/extdata/nm_clim.prn")
-# table <- table[-1, ]
-# names(table) <- names
-# table_long <- tidyr::pivot_longer(table, cols = -year, names_to = "month")
-#
-#
-#
-# check <- cbind(clim_long, table_long$value) %>%
-#   mutate(check = clim_long$log_clim == table_long$value)
-#
-# names <- c("year", "month", "value")
-# colnames(clim_long) <- names
-
-
-## average climate
-
-climate <- load_clim(clim = climate, mos = -8:7, method = "mean", prewhiten.clim = FALSE)
-
-## eval clim
-
-data <- eval_clim(crns = crns_df, lead = 1, lag = 1, prewhiten.crn = TRUE, climate = climate, calib = 1928:1945, valid = 1946:1972, cor.window = "calib", type = "pearson", alternative = "two.sided", r = -1, alpha = 0.9, print.out = TRUE, save.out = "csv", dir = "test/")
+data <- eval_clim(crns = crns_df, lead = 1, lag = 1, prewhiten.crn = TRUE, climate = clim_long, mos = 5:8, method = "mean", prewhiten.clim = TRUE, calib = c(1927:1945, 1958:1969), valid = 1946:1957, cor.window = "calib", type = "pearson", alternative = "two.sided", r = -1, alpha = 0.9, print.out = TRUE, save.out = "csv", dir = "test/")
 
 
 ## run PCreg function!
@@ -90,7 +55,7 @@ ggplot2::ggplot(recon_filter) +
   #geom_ribbon(aes(ymin = lwr, ymax = upr, x = year), fill = "gray50", alpha = 0.5) +
   ggplot2::geom_line(ggplot2::aes(y = reds, x = year), color = "blue") +
   ggplot2::geom_line(data = lm_chck, ggplot2::aes(y = X1, x = year), color = "red")
-  #ggplot2::geom_line(ggplot2::aes(y = reds, x = year), color = "black")
+#ggplot2::geom_line(ggplot2::aes(y = reds, x = year), color = "black")
 
 npw <- ar(pcreg_red)
 red <- ar(pcreg_npw)
@@ -113,15 +78,18 @@ valid <- 1946:1972
 print.out <- TRUE
 save.out <- "table"
 dir <- "PCregOutput/"
+i <- 1
+j <- 1
+lag <- 1
+
 
 pc.calc <- "calib"
 select.pc <- "eigenvalue1"
 scale.var <- 'calib'
 weight <- NULL
-i <- 1
 
 
 read_in <- colnames(crns_df[ ,-1])
 
-didnt_load <- select_crns_rad[!stringr::str_detect(select_crns_rad, regex(paste(read_in, collapse = "|"), ignore_case = TRUE))]
+didnt_load <- select_crns_rad[!stringr::str_detect(select_crns_rad, stringr::regex(paste(read_in, collapse = "|"), ignore_case = TRUE))]
 
